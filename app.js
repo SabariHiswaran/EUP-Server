ae07b2e56a478c48730840902196c7940d5aaf91
Sabari <sabarihiswaran555@gmail.com>
2023-08-14 20:34:33 +0530

    db connection made with mongoose

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 
 const express = require('express')
 
+const mongoose = require('mongoose')
+
 const app = express()
 
 app.get('/' , (req,res,next) => {
-    res.send("Server Connected")
+    res.send("Server is Connected")
     next()
 })
 
-app.listen(5000)
+mongoose.connect("mongodb+srv://sabari:2aHFuH3s4CiIMXYf@cluster0.d9vjeat.mongodb.net/test?retryWrites=true&w=majority")
+.then(() => app.listen(5000))
+.catch((err) => {
+    console.log(err)
+})
