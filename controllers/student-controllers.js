df775ff487787283cb6636b90798d22bcea042d9
Sabari <sabarihiswaran555@gmail.com>
2023-09-04 10:30:34 +0530

    student controllers and routes modified

--- controllers/student-controllers.js
+++ controllers/student-controllers.js
@@ -1,6 +1,7 @@
 
 const HttpError = require('../model/Http-error')
 const Course = require('../model/course-model')
+const CourseMeeting = require("../model/meeting-model")
 
 const courselist = async (req,res,next) => {
 
@@ -17,5 +18,25 @@ const courselist = async (req,res,next) => {
     res.json({courses : course})
 }
 
+const upcomingMeetings = async (req,res,next) => {
+
+  const {courseTopic,topic} = req.params
+
+  
+  let meetings;
+
+  try{
+    meetings =  await CourseMeeting.find({courseTopic : courseTopic,topic : topic})
+    convertedCourseMeetings = meetings.map(meeting => meeting.toObject({getters : true }))
+  }catch(err){
+      const error = new HttpError("Cannot find the Upcoming meetings for the selected course" , 404)
+      return next(error)
+  }
+
+  res.json({meetings : convertedCourseMeetings})
+
+}
+
 
 exports.courselist = courselist
+exports.upcomingMeetings = upcomingMeetings
--- routes/student-routes.js
+++ routes/student-routes.js
@@ -5,4 +5,6 @@ const router = express.Router()
 
 router.get("/courses" , studentControllers.courselist)
 
+router.get("/courses/:courseTopic/:topic/upcomingMeetings",studentControllers.upcomingMeetings)
+
 module.exports = router
